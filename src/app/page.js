4643ad6ffdd08bcc122a1de0c048380a779d0ffd
Arturocs160 <kronculd@outlook.com>
2024-11-07 14:40:57 -0600

    init commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,119 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// Componente funcional
 
-export default function Home() {
+// Función flecha
+// Función anónima
+// Función de expresión
+// Función de asignación
+// IIEF (Inmediately Invoked Function Expresion) Función de expresión invocada inmediatamente
+
+// Función de expresión
+
+"use client";
+import { useState } from "react"
+import styles from "./page.module.css"
+
+export default function Page() {
+
+  const [tareas, setTareas] = useState([]);
+  const [filtroTexto, setfiltroTexto] = useState("");
+  const [filtroPrioridad, setfiltroPrioridad] = useState("");
+
+  const [nuevaTarea, setNuevaTarea] = useState({
+    nombre: "",
+    fecha: "",
+    prioridad: ""
+  })
+  console.log(nuevaTarea)
+  console.log(tareas)
+
+  function handleChange(event) {
+    setNuevaTarea({
+      ...nuevaTarea,
+      [event.target.name]: event.target.value
+    })
+  }
+  
+  function handleChangeSearchText(event) {
+    setfiltroTexto(event.target.value)
+  }
+
+  function handleChangeSearchPrioridad(event) {
+    setfiltroPrioridad(event.target.value)
+  }
+
+  function agregarTarea() {
+    const newListaTareas = tareas.slice();
+    const newTarea = {
+      nombre: nuevaTarea.nombre,
+      fecha: nuevaTarea.fecha,
+      prioridad: nuevaTarea.prioridad,
+      creadoEl: new Date().toISOString()
+    }
+    newListaTareas.push(newTarea);
+    setTareas(newListaTareas);
+    setNuevaTarea({
+      nombre: "",
+      fecha: "",
+      prioridad: ""
+    })
+  }
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <h1>Todo-list</h1>
+        <input value={nuevaTarea.nombre} onChange={handleChange} type="text" placeholder="Agregar tarea..." name="nombre"/>
+        <input value={nuevaTarea.fecha} onChange={handleChange} type="date" name="fecha"/>
+        <select name="prioridad" onChange={handleChange} value={nuevaTarea.prioridad}>
+          <option>Prioridad</option>
+          <option>Alta</option>
+          <option>Media</option>
+          <option>Baja</option>
+        </select>
+        <button onClick={agregarTarea}>Agregar</button>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div style={{marginTop: "20px"}}>
+          <h4>Filtros</h4>
+          <input  value={filtroTexto} onChange={handleChangeSearchText} className={styles.busqueda} type="text" placeholder="Buscar tarea..."></input>
+
+          <div>
+            <p>Ordenar por prioridad</p>
+            <select className={styles.busqueda} onChange={handleChangeSearchPrioridad} value={filtroPrioridad}>
+              <option>Prioridad</option>
+              <option>Alta</option>
+              <option>Media</option>
+              <option>Baja</option>
+            </select>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        
+        <div style={{marginTop: "20px"}}>
+          <ul>
+            {
+              tareas
+              .filter((tarea) => tarea.nombre.toLowerCase().includes(filtroTexto.toLowerCase()))
+              .filter(tarea => {
+                if (filtroPrioridad === "") {
+                  return true;
+                }
+                return tarea.prioridad === filtroPrioridad;
+              })
+              .map(
+                (tarea) => {
+                  return(
+                    <li className={styles.tarea}>
+                      <h4>{tarea.nombre}</h4>
+                      <p>{tarea.fecha}</p>
+                      <p>{tarea.prioridad}</p>
+                    </li>
+                  )
+                }
+              )
+            }
+          </ul>
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
